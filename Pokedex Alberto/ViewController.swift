9e95260e145ef019a031d1fc21614aec91d4be83
Juan Antonio Carrasco del Cid <juan.carrascodelcid@plexus.es>
2023-11-20 12:38:12 +0100

    Initial Commit

---
+++ Pokedex Alberto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex Alberto
+//
+//  Created by Juan Antonio Carrasco del Cid on 20/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
